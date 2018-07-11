b3d612d10db5c28e102ff03d408a5bcf8e4f9b24
OmerUnger <omerunger7@gmail.com>
2018-07-11 13:22:30 +0300

    add snowboy

--- thevoice/App.js
+++ thevoice/App.js
@@ -13,12 +13,39 @@ import {
   Button
 } from 'react-native';
 import RNImmediatePhoneCall from 'react-native-immediate-phone-call';
+import snowboy from 'react-native-snowboy';
+
+
+snowboy.initHotword()
+	.then((res)=> {
+		console.log(res)
+	})
+	.catch((err)=> {
+		console.log(err)
+	})
+
+// No hotword detected
+	snowboy.addEventListener("msg-vad-speech", (e) => {
+		console.log('msg-vad-speech',e)
+	})
+	
+	// No speech: silence
+	snowboy.addEventListener("msg-vad-nospeech", (e) => {
+		console.log('msg-vad-nospeech',e)
+	})
+
+// The hotword is decteced
+	snowboy.addEventListener("msg-active", (e) => {
+		console.log('msg-active',e)
+	})
+
+//RNImmediatePhoneCall.immediatePhoneCall('0542220858');
 
 export default class App extends Component<Props> {
   render() {
     return (
       <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-        <Button onPress={() => {RNImmediatePhoneCall.immediatePhoneCall('0542220858');}} title="Record"/>
+        <Button onPress={() => {snowboy.startRecording();}} title="Record"/>
       </View>
     );
   }
